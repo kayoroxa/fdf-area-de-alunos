0eba85cb600acba72ea8646c2f440ce0cefb5b04
Caio <kayoroxa@gmail.com>
2022-10-21 20:14:33 -0300

    fix api

--- src/pages/api/modules/fake/index.ts
+++ src/pages/api/modules/fake/index.ts
@@ -8,17 +8,19 @@ export default async function handler(
     res.status(200).json([
       {
         slug: 'seu-pattern',
-        name: 'F Seu Pattern',
+        name: 'Seu Pattern',
         imgUrl:
           'https://images.prismic.io/fdf-area-de-alunos/b4753ee3-e6e0-466d-91a3-fa7322a10804_2.png?auto=compress,format',
         allow: true,
         lessons: [
           {
+            id: 'https://www.w3schools.com/html/mov_bbb.mp4',
             name: 'Parte 1 - Nivel 0',
             allow: true,
             videoUrl: 'https://www.w3schools.com/html/mov_bbb.mp4',
           },
           {
+            id: 'https://www.w3schools.com/html/mov_bbb.mp4',
             name: 'Parte 2 - Nivel 0',
             allow: true,
             videoUrl: 'https://www.w3schools.com/html/mov_bbb.mp4',
@@ -33,18 +35,21 @@ export default async function handler(
         allow: true,
         lessons: [
           {
+            id: '17nyS2ZYEd6xaUqANv4FDNawn-4OS3Dut',
             name: 'Aprenda verbo to be em 8 minutos',
             allow: true,
             videoUrl:
               'https://docs.google.com/uc?export=open&id=17nyS2ZYEd6xaUqANv4FDNawn-4OS3Dut',
           },
           {
+            id: '1_apBEcdDTwhjd6tteBluxnwY5Lc9q_1Y',
             name: 'Códigos rápido',
             allow: true,
             videoUrl:
               'https://docs.google.com/uc?export=open&id=1_apBEcdDTwhjd6tteBluxnwY5Lc9q_1Y',
           },
           {
+            id: '1oHFs2sghIyQ3FQcrkRiMH6iSmdilNfuD',
             name: 'Can e Shall',
             allow: true,
             videoUrl:
