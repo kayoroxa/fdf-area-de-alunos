d3b80a31956fea0a8a57de1b099a96ae835ef50d
Caio <kayoroxa@gmail.com>
2023-01-06 03:47:37 -0300

    hand hello name

--- src/molecules/HeadWelcome.tsx
+++ src/molecules/HeadWelcome.tsx
@@ -1,3 +1,4 @@
+import { FaHandPeace } from 'react-icons/fa'
 import useUsers from '../services/hooks/useUsers'
 
 export default function HeaderWelcome() {
@@ -6,9 +7,11 @@ export default function HeaderWelcome() {
   return (
     <div id="user-info" className="p-4">
       <h3>Hello,</h3>
-      <div className="relative w-max flex">
+      <div className="relative w-max flex gap-2">
         <h1 className="text-3xl font-bold">{data?.name}</h1>
-        <span className="-mt-6 text-6xl bottom-0">👋🏽</span>
+        <span className="-mt-6 text-3xl bottom-0 flex items-end">
+          <FaHandPeace />
+        </span>
       </div>
     </div>
   )
