fb06af0fa0942342d54872482793f17999f84061
Caio <kayoroxa@gmail.com>
2023-01-06 03:47:53 -0300

    video fake wait

--- src/atoms/Video.tsx
+++ src/atoms/Video.tsx
@@ -1,23 +1,40 @@
 /* eslint-disable @next/next/no-img-element */
 import { useEffect, useRef, useState, VideoHTMLAttributes } from 'react'
+import { GoPlay } from 'react-icons/go'
 // import loading from 'loading.gif'
 
 interface IProps extends VideoHTMLAttributes<HTMLVideoElement> {
   loading?: boolean
 }
 
-export default function Video({ src, loading }: IProps) {
+export default function Video({ src }: IProps) {
   const [loaded, setLoaded] = useState(false)
 
+  const videoRef = useRef<HTMLVideoElement>(null)
+  const loadingRef = useRef<HTMLImageElement>(null)
+  const [showBack, setShowBack] = useState(true)
+  const [showPlay, setShowPlay] = useState(false)
+
   useEffect(() => {
+    setShowBack(true)
     if (videoRef.current && src) {
       videoRef.current.src = src
     }
     setLoaded(false)
+    setShowPlay(false)
+
     if (loadingRef.current)
       loadingRef.current.src =
         loadingRef.current.src.replace(/\?.*$/, '') + `?x=${Math.random()}`
+
+    setTimeout(() => {
+      setShowPlay(true)
+    }, 500)
   }, [src])
+
+  useEffect(() => {
+    if (!showBack) setShowPlay(false)
+  }, [showBack])
 
   // useEffect(() => {
   //   if (loadingRef.current)
@@ -25,12 +42,9 @@ export default function Video({ src, loading }: IProps) {
   //       loadingRef.current.src.replace(/\?.*$/, '') + `?x=${Math.random()}`
   // }, [loaded])
 
-  const videoRef = useRef<HTMLVideoElement>(null)
-  const loadingRef = useRef<HTMLImageElement>(null)
-
   return (
     <div id="videoWrapper" className="w-full relative bg-black p-video">
-      {!loaded && (
+      {!loaded && !showBack && (
         <>
           <p className="absolute bottom-[20%] z-20 left-0 right-0 m-auto w-fit text-xl">
             Carregando a aula...
@@ -42,6 +56,20 @@ export default function Video({ src, loading }: IProps) {
             className="h-4 w-full min-h-full min-w-full object-cover absolute top-0"
           /> */}
         </>
+      )}
+
+      {showBack && !loaded && (
+        <div
+          onClick={() => setShowBack(false)}
+          className="absolute z-10 bg-zinc-900/95 w-full h-full top-0 flex justify-center items-center"
+        >
+          {showPlay && (
+            <GoPlay
+              size={90}
+              className="hover:cursor-pointer hover:scale-105 hover:text-blue-300"
+            />
+          )}
+        </div>
       )}
 
       {/* <div className="w-full h-6 bg-gray-200 rounded-full dark:bg-gray-700">
